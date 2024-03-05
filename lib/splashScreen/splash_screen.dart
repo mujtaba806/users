fd26ae52fc09b9cc25d8240fadf0f7a13a3da0d9
Mujtaba <mujtabaghulam24682gmail.com>
2024-03-05 12:36:57 +0500

    abhi wala

--- lib/splashScreen/splash_screen.dart
+++ lib/splashScreen/splash_screen.dart
@@ -6,42 +6,48 @@ import 'package:users/global/global.dart';
 import 'package:users/screens/login_screen.dart';
 import 'package:users/screens/main_screen.dart';
 
+//sfsdfsdddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd
 class SplashScreen extends StatefulWidget {
   const SplashScreen({Key? key}) : super(key: key);
 
   @override
-  State<SplashScreen>createState()=> _SplashScreenState();
+  State<SplashScreen> createState() => _SplashScreenState();
 }
-class _SplashScreenState extends State<SplashScreen>{
 
-  startTimer(){
-    Timer(Duration(seconds: 3), ()async {
-      if(await firebaseAuth.currentUser!=null){
-        firebaseAuth.currentUser!=null?AssistantMethods.readCurrentOnlineUserInfo():null;
-        Navigator.push(context,MaterialPageRoute(builder:(c)=> MainScreen()));
-      }
-      else{
-        Navigator.push(context, MaterialPageRoute(builder:(c)=> LoginScreen()));
+class _SplashScreenState extends State<SplashScreen> {
+  startTimer() {
+    Timer(Duration(seconds: 3), () async {
+      if (await firebaseAuth.currentUser != null) {
+        firebaseAuth.currentUser != null
+            ? AssistantMethods.readCurrentOnlineUserInfo()
+            : null;
+        Navigator.push(
+            context, MaterialPageRoute(builder: (c) => MainScreen()));
+      } else {
+        Navigator.push(
+            context, MaterialPageRoute(builder: (c) => LoginScreen()));
       }
     });
   }
+
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
     startTimer();
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body:Center(
-        child:Text('VDrive',
-        style: TextStyle(
+      body: Center(
+        child: Text(
+          'VDrive',
+          style: TextStyle(
             fontSize: 40,
-          fontWeight: FontWeight.bold,
+            fontWeight: FontWeight.bold,
+          ),
         ),
-        ),
-
       ),
     );
   }
