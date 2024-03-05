07fabfef80816bdbdabae01ca2a711f2b4e0a5b0
Faizan <faizanmoazam3@gmai;.com>
2024-03-05 12:57:53 +0500

    ghin

--- lib/splashScreen/splash_screen.dart
+++ lib/splashScreen/splash_screen.dart
@@ -6,48 +6,42 @@ import 'package:users/global/global.dart';
 import 'package:users/screens/login_screen.dart';
 import 'package:users/screens/main_screen.dart';
 
-//sfsdfsdddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd
 class SplashScreen extends StatefulWidget {
   const SplashScreen({Key? key}) : super(key: key);
 
   @override
-  State<SplashScreen> createState() => _SplashScreenState();
+  State<SplashScreen>createState()=> _SplashScreenState();
 }
+class _SplashScreenState extends State<SplashScreen>{
 
-class _SplashScreenState extends State<SplashScreen> {
-  startTimer() {
-    Timer(Duration(seconds: 3), () async {
-      if (await firebaseAuth.currentUser != null) {
-        firebaseAuth.currentUser != null
-            ? AssistantMethods.readCurrentOnlineUserInfo()
-            : null;
-        Navigator.push(
-            context, MaterialPageRoute(builder: (c) => MainScreen()));
-      } else {
-        Navigator.push(
-            context, MaterialPageRoute(builder: (c) => LoginScreen()));
+  startTimer(){
+    Timer(Duration(seconds: 3), ()async {
+      if(await firebaseAuth.currentUser!=null){
+        firebaseAuth.currentUser!=null?AssistantMethods.readCurrentOnlineUserInfo():null;
+        Navigator.push(context,MaterialPageRoute(builder:(c)=> MainScreen()));
+      }
+      else{
+        Navigator.push(context, MaterialPageRoute(builder:(c)=> LoginScreen()));
       }
     });
   }
-
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
     startTimer();
   }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text(
-          'VDrive',
-          style: TextStyle(
-            fontSize: 40,
-            fontWeight: FontWeight.bold,
-          ),
+      body:Center(
+        child:Text('VDrive',
+        style: TextStyle(
+            fontSize: 42,
+          fontWeight: FontWeight.bold,
         ),
+        ),
+
       ),
     );
   }
