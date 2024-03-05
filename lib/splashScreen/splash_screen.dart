9b87ab8803b1889ef8dc9bd4ae64ac9d6a5e6b9d
Mujtaba <mujtabaghulam24682gmail.com>
2024-03-05 12:59:37 +0500

    xz

--- lib/splashScreen/splash_screen.dart
+++ lib/splashScreen/splash_screen.dart
@@ -10,38 +10,43 @@ class SplashScreen extends StatefulWidget {
   const SplashScreen({Key? key}) : super(key: key);
 
   @override
-  State<SplashScreen>createState()=> _SplashScreenState();
+  State<SplashScreen> createState() => _SplashScreenState();
 }
-class _SplashScreenState extends State<SplashScreen>{
 
-  startTimer(){
-    Timer(Duration(seconds: 3), ()async {
-      if(await firebaseAuth.currentUser!=null){
-        firebaseAuth.currentUser!=null?AssistantMethods.readCurrentOnlineUserInfo():null;
-        Navigator.push(context,MaterialPageRoute(builder:(c)=> MainScreen()));
-      }
-      else{
-        Navigator.push(context, MaterialPageRoute(builder:(c)=> LoginScreen()));
+class _SplashScreenState extends State<SplashScreen> {
+  startTimer() {
+    Timer(Duration(seconds: 3), () async {
+      if (await firebaseAuth.currentUser != null) {
+        firebaseAuth.currentUser != null
+            ? AssistantMethods.readCurrentOnlineUserInfo()
+            : null;
+        Navigator.push(
+            context, MaterialPageRoute(builder: (c) => MainScreen()));
+      } else {
+        Navigator.push(
+            context, MaterialPageRoute(builder: (c) => LoginScreen()));
       }
     });
   }
+
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
     startTimer();
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body:Center(
-        child:Text('VDrive',
-        style: TextStyle(
-            fontSize: 42,
-          fontWeight: FontWeight.bold,
+      body: Center(
+        child: Text(
+          'VDrive',
+          style: TextStyle(
+            fontSize: 43,
+            fontWeight: FontWeight.bold,
+          ),
         ),
-        ),
-
       ),
     );
   }
