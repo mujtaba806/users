e28ccd136e3060e52c8058f529342f6ac50ae5ec
Mujtaba <mujtabaghulam24682gmail.com>
2024-03-07 11:43:14 +0500

    abhi walaa

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -1,9 +1,13 @@
- import 'dart:async';
+import 'dart:async';
+import 'package:geocoder2/geocoder2.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:location/location.dart' as loc;
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:users/Assistant/assistant_method.dart';
+import 'package:users/global/global.dart';
+
+import '../global/map_key.dart';
 
 class MainScreen extends StatefulWidget {
   const MainScreen({Key? key}) : super(key: key);
@@ -61,6 +65,35 @@ class _MainScreenState extends State<MainScreen> {
         await AssistantMethods.searchAddressForGeographicCoordinates(
             userCurrentPosition!, context);
     print("This is our address" + humanReadableAddress);
+    userName = userModelCurrentInfo!.name!;
+  }
+
+  getAddressFromLatLng() async {
+    try {
+      GeoData data = await Geocoder2.getDataFromCoordinates(
+          latitude: pickLocation!.latitude,
+          longitude: pickLocation!.longitude,
+          googleMapApiKey: mapKey);
+      setState(() {
+        _address = data.address;
+      });
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  checkIfLocationPermissionAllowed() async {
+    _locationPermission = await Geolocator.requestPermission();
+    if (_locationPermission == LocationPermission.denied) {
+      _locationPermission = await Geolocator.requestPermission();
+    }
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    checkIfLocationPermissionAllowed();
   }
 
   @override
@@ -75,7 +108,7 @@ class _MainScreenState extends State<MainScreen> {
             GoogleMap(
               initialCameraPosition: _kGooglePlex,
               mapType: MapType.normal,
-              myLocationButtonEnabled: true,
+              myLocationEnabled: true,
               zoomGesturesEnabled: true,
               zoomControlsEnabled: true,
               polylines: polylineSet,
@@ -95,8 +128,35 @@ class _MainScreenState extends State<MainScreen> {
                 }
               },
               onCameraIdle: () {
-                // getAddressFromLatLng();
+                getAddressFromLatLng();
               },
+            ),
+            Align(
+              alignment: Alignment.center,
+              child: Padding(
+                padding: EdgeInsets.only(bottom: 35.0),
+                child: Image.network(
+                  'images/location.png',
+                  height: 45,
+                  width: 45,
+                ),
+              ),
+            ),
+            Positioned(
+              top: 40,
+              right: 20,
+              left: 20,
+              child: Container(
+                decoration: BoxDecoration(
+                    border: Border.all(color: Colors.black),
+                    color: Colors.white),
+                padding: EdgeInsets.all(20),
+                child: Text(
+                  _address ?? "Set your pickuplocation",
+                  overflow: TextOverflow.visible,
+                  softWrap: true,
+                ),
+              ),
             )
           ],
         ),
