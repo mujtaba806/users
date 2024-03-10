aefa283cd6281f2d5d1127dc2252d77c0cc714cd
Faizan <faizanmoazam3@gmai;.com>
2024-03-10 18:03:52 +0500

    pt.8 done

--- lib/screens/drawer_screen.dart
+++ lib/screens/drawer_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:users/global/global.dart';
+import 'package:users/screens/profile_screen.dart';
+import 'package:users/splashScreen/splash_screen.dart';
 
 class DrawerScreen extends StatelessWidget {
   const DrawerScreen({super.key});
@@ -11,7 +14,86 @@ class DrawerScreen extends StatelessWidget {
         child: Padding(
           padding: EdgeInsets.fromLTRB(30, 50, 0, 20),
           child: Column(
-            children: [],
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Container(
+                    padding: EdgeInsets.all(30),
+                    decoration: BoxDecoration(
+                      color: Colors.lightBlue,
+                      shape: BoxShape.circle,
+                    ),
+                    child:Icon(
+                      Icons.person,
+                      color: Colors.white,
+                    ),
+                  ),
+                  SizedBox(height: 20,),
+                  
+                  Text(
+                    userModelCurrentInfo!.name!,
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20,
+                    ),
+                  ),
+                  SizedBox(height: 10,),
+
+                  GestureDetector(
+                    onTap: (){
+                      Navigator.push(context, MaterialPageRoute(builder: (c) =>ProfileScreen()));
+                    },
+                    child: Text(
+                      "Edit Profile",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 15,
+                        color: Colors.blue
+                      ),
+                    ),
+                  ),
+
+                  SizedBox(height: 30,),
+                  Text("Your Trips" , style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),),
+
+                  SizedBox(height: 30,),
+                  Text("Payement" , style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),),
+
+                  SizedBox(height: 30,),
+                  Text("Notifications" , style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),),
+
+                  SizedBox(height: 30,),
+                  Text("Promos" , style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),),
+
+                  SizedBox(height: 30,),
+                  Text("Help" , style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),),
+
+                  SizedBox(height: 30,),
+                  Text("Free Trips" , style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),),
+
+                  SizedBox(height: 30,),
+
+                ],
+              ),
+
+              GestureDetector(
+                onTap: (){
+                  firebaseAuth.signOut();
+                  Navigator.push(context, MaterialPageRoute(builder: (c) =>SplashScreen()));
+                },
+                child: Text(
+                  "Logout",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 18,
+                    color: Colors.red,
+                  ),
+                ),
+              )
+            ],
           ),
         ),
       ),
